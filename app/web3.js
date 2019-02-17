715ca6801cfc0a80a3836239565c439878200661
Mike Calvanese <mfcalvanese@gmail.com>
2019-02-17 04:32:21 -0700

    downgrade web3 and add provider

--- app/web3.js
+++ app/web3.js
@@ -2,7 +2,7 @@ import Web3 from 'web3';
 
 let web3 = "undefined"
 try {
-	web3 = new Web3(window.web3.currentProvider);
+	web3 = new Web3('http://localhost:8545');
 } catch(err) {
 	web3 = "undefined";
 }
