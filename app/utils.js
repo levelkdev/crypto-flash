50cb8611db50961076f685b1ce0e51c1a2be27e8
Mike Calvanese <mfcalvanese@gmail.com>
2019-02-17 03:10:32 -0700

    comment TMP storage values

--- app/utils.js
+++ app/utils.js
@@ -24,10 +24,9 @@ async function getCredentials() {
   }
 
   // TMP
-  deviceAddress = '0xdf08f82de32b8d460adbe8d72043e3a7e25a3b39'
-  privateKey = '0x2bdd21761a483f71054e14f5b827213567971c676928d9a1808cbfa4b7501200'
-
-  walletContract = await Account.at('0xF9989C011baA56b65542ad4E629A52eeEe6E3bFb')
+  // deviceAddress = '0xdf08f82de32b8d460adbe8d72043e3a7e25a3b39'
+  // privateKey = '0x2bdd21761a483f71054e14f5b827213567971c676928d9a1808cbfa4b7501200'
+  // walletContract = await Account.at('0x0f5Ea0A652E851678Ebf77B69484bFcD31F9459B')
 
   return { privateKey, deviceAddress, walletContract }
 }
