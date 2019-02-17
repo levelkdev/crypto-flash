384ec49ef0baca185751c31e9340301d8137acb9
Mike Calvanese <mfcalvanese@gmail.com>
2019-02-17 04:04:22 -0700

    get more localstorage values

--- app/utils.js
+++ app/utils.js
@@ -4,7 +4,7 @@ import Account from './web3Contracts/Account'
 // const developmentKey = '0x2bdd21761a483f71054e14f5b827213567971c676928d9a1808cbfa4b7501200'
 
 async function getCredentials() {
-  let privateKey, deviceAddress, ensName, walletContract
+  let privateKey, deviceAddress, ensName, walletAddress, walletContract
 
   if (localStorage.getItem('key')) {
     privateKey = localStorage.getItem('key')
@@ -18,17 +18,18 @@ async function getCredentials() {
   }
 
   ensName = localStorage.getItem('ensName') || null
-
-  if (ensName) {
-    // TODO: get walletContract from ensName
+  walletAddress = localStorage.getItem('walletAddress')
+  if (walletAddress) {
+    walletContract = await Account.at(walletAddress)
   }
 
-  // TMP
-  // deviceAddress = '0xdf08f82de32b8d460adbe8d72043e3a7e25a3b39'
-  // privateKey = '0x2bdd21761a483f71054e14f5b827213567971c676928d9a1808cbfa4b7501200'
-  // walletContract = await Account.at('0x0f5Ea0A652E851678Ebf77B69484bFcD31F9459B')
-
-  return { privateKey, deviceAddress, walletContract }
+  return {
+    privateKey,
+    deviceAddress,
+    walletAddress,
+    walletContract,
+    ensName
+  }
 }
 
 export default getCredentials
